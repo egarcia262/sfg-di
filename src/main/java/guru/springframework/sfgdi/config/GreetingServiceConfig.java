3dc5559a061eda6164ad18759ed5c842daaffc58
Edu <garcia_eduher@gva.es>
2022-07-04 14:02:03 +0200

    Refactoring annotations in a config file

---
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -0,0 +1,51 @@
+package guru.springframework.sfgdi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+
+import guru.springframework.sfgdi.services.ConstructorGreetingService;
+import guru.springframework.sfgdi.services.I18nEnglishGreetingService;
+import guru.springframework.sfgdi.services.I18nSpanishService;
+import guru.springframework.sfgdi.services.PrimaryGreetingService;
+import guru.springframework.sfgdi.services.PropertyGreetingService;
+import guru.springframework.sfgdi.services.SetterGreetingService;
+
+@Configuration
+public class GreetingServiceConfig {
+
+	@Profile({"ES", "default"})
+    @Bean("i18nService")
+    I18nSpanishService I18nSpanishService(){
+        return new I18nSpanishService();
+    }
+
+    @Profile("EN")
+    @Bean
+    I18nEnglishGreetingService i18nService(){
+        return new I18nEnglishGreetingService();
+    }
+
+    @Primary
+    @Bean
+    PrimaryGreetingService primaryGreetingService(){
+        return new PrimaryGreetingService();
+    }
+
+    @Bean
+    ConstructorGreetingService constructorGreetingService(){
+        return new ConstructorGreetingService();
+    }
+
+    @Bean
+    PropertyGreetingService propertyGreetingService(){
+        return new PropertyGreetingService();
+    }
+
+    @Bean
+    SetterGreetingService setterGreetingService(){
+        return new SetterGreetingService();
+    }
+
+}
